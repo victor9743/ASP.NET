d95d8cfa16fbe08fe91fcf3c8b8626cb1853914c
Victor <victorcosta@gmail.com>
2021-08-20 18:39:22 +0100

    working with four verbs

---
+++ Calculadora/Calculadora/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Calculadora.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+     
+
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("soma/{num1}/{num2}")]
+        public IActionResult soma(string num1, string num2)
+        {
+
+            return BadRequest("Valor Inválido");
+            
+        }
+
+        
+
+
+
+    }
+}
---
+++ Calculadora/Calculadora/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculadora.Model
+{
+    public class Person
+    {
+        public long id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string endereco { get; set; }
+        public string gender { get; set; }
+
+    }
+}
